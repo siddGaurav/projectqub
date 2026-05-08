799cd580a5e7d7a294221f8582f03dd05d758afd
Reenu409 <reenukumari3435@gmail.com>
2026-05-08 22:48:42 +0530

    Fix robots export

--- src/app/robots.tsx
+++ src/app/robots.tsx
@@ -1,4 +1,25 @@
+// import type { MetadataRoute } from "next";
+
+// export default function robots(): MetadataRoute.Robots {
+
+//   return {
+
+//     rules: {
+
+//       userAgent: "*",
+
+//       allow: "/",
+//     },
+
+//     sitemap:
+//       "https://qubnix.com/sitemap.xml",
+//   };
+// }
+
+
 import type { MetadataRoute } from "next";
+
+export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
 
