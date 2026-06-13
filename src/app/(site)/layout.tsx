2fb4ccad5549203201561b6b7e8af06c94fa72fe
siddGaurav <gauravsiddharth.mj@gmail.com>
2026-06-13 14:02:09 +0530

    Fix ThemeProvider for site layout

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
+import { AppProviders } from "@/components/providers/AppProviders";
 
 export default function SiteLayout({
   children,
@@ -7,12 +8,16 @@ export default function SiteLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <AppProviders>
       <Navbar />
+
       <div className="flex min-h-screen flex-col pt-[5.25rem]">
-        <main className="relative z-[20] flex-1">{children}</main>
+        <main className="relative z-[20] flex-1">
+          {children}
+        </main>
+
         <Footer />
       </div>
-    </>
+    </AppProviders>
   );
 }
